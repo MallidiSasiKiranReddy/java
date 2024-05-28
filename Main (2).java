8f18175b74b9492a1c656813955b00db2cadcf3c
Sasiingit <114077036+Sasiingit@users.noreply.github.com>
2024-05-28 15:58:12 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main
+{
+    public static void main(String []args)
+    {
+        Queue<Integer> q=new ArrayDeque<Integer>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        System.out.println("Initial Queue:"+q);
+        Stack<Integer> stack=new Stack<Integer>();
+        while(!q.isEmpty())
+        {
+            stack.push(q.poll());
+        }
+        System.out.println("Stack:"+stack);
+        while(!stack.isEmpty())
+        {
+            q.add(stack.pop());
+        }
+        System.out.println("Reversed Queue"+q);
+        
+    }
+}
