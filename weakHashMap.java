921ef238fac49f35f142bc949dd06ea7f55fefad
Sasiingit <114077036+Sasiingit@users.noreply.github.com>
2024-05-29 11:13:36 +0530

    Create weakHashMap.java

---
+++ weakHashMap.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class weakHashMap
+{
+public static void main(String []args)
+{
+WeakHashMap<String,Integer> whm=new WeakHashMap<String,Integer>();
+whm.put("ONE",1);
+whm.put("TWO",2);
+whm.put("THREE",3);
+System.out.println(whm);
+String four=new String("FOUR");
+Integer fourValue=4;
+whm.put(four,fourValue);
+System.out.println(whm);
+four=null;
+System.gc();
+System.out.println(whm);
+}
+}
